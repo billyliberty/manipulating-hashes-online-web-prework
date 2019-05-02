0535b7be717398e473254309ca5b2391d1528420
Joshua Crum <joshua.crum@gmail.com>
2019-05-02 17:21:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,8 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        value.each do |flavor|
-          delete_if? value == "strawberry"
+        value.delete_if? do |flavor|
+          value == "strawberry"
         end
       end
     end
